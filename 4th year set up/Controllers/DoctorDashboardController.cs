8a0ded87f38e440c677e83826ef3d5a77c9f0a08
Khutsiso <Khutsisoletageng7@gmail.com>
2026-05-22 15:27:07 +0200

    draai

--- 4th year set up/Controllers/DoctorDashboardController.cs
+++ 4th year set up/Controllers/DoctorDashboardController.cs
@@ -9,6 +9,35 @@ namespace _4th_year_set_up.Controllers
             ViewBag.Email = HttpContext.Session.GetString("Email") ?? "dev-doctor@test.com";
             return View("~/Views/Doctor/DoctorDashboard.cshtml");
         }
-       
+
+        public IActionResult PatientRecords()
+        {
+            ViewBag.Email = HttpContext.Session.GetString("Email") ?? "dev-doctor@test.com";
+            return View("~/Views/Doctor/PatientRecords.cshtml");
+        }
+
+        public IActionResult TestRequests()
+        {
+            ViewBag.Email = HttpContext.Session.GetString("Email") ?? "dev-doctor@test.com";
+            return View("~/Views/Doctor/TestRequests.cshtml");
+        }
+
+        public IActionResult ViewResults()
+        {
+            ViewBag.Email = HttpContext.Session.GetString("Email") ?? "dev-doctor@test.com";
+            return View("~/Views/Doctor/ViewResults.cshtml");
+        }
+
+        public IActionResult Reports()
+        {
+            ViewBag.Email = HttpContext.Session.GetString("Email") ?? "dev-doctor@test.com";
+            return View("~/Views/Doctor/Reports.cshtml");
+        }
+
+        public IActionResult Alerts()
+        {
+            ViewBag.Email = HttpContext.Session.GetString("Email") ?? "dev-doctor@test.com";
+            return View("~/Views/Doctor/Alerts.cshtml");
+        }
     }
 }
