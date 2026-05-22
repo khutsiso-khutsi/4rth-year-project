861d2c4eff433be662d03043ded843443b39286e
Khutsiso <Khutsisoletageng7@gmail.com>
2026-05-22 14:22:34 +0200

    draai

--- 4th year set up/Controllers/DoctorDashboardController.cs
+++ 4th year set up/Controllers/DoctorDashboardController.cs
@@ -9,5 +9,6 @@ namespace _4th_year_set_up.Controllers
             ViewBag.Email = HttpContext.Session.GetString("Email") ?? "dev-doctor@test.com";
             return View("~/Views/Doctor/DoctorDashboard.cshtml");
         }
+       
     }
 }
---
+++ 4th year set up/Models/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _4th_year_set_up.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class UserSession
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+        public string FullName { get; set; }
+        public int RoleID { get; set; }
+    }
+}
